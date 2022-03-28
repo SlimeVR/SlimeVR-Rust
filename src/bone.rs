7a44678064a8dc5aa9d4f375135a0d8a5c6c51dd
Ryan Butler <thebutlah@gmail.com>
2022-03-28 02:24:05 -0400

    No longer using hack for bone length

--- src/bone.rs
+++ src/bone.rs
@@ -1,11 +1,10 @@
-use std::collections::HashMap;
-
 use crate::RGBA;
 
 use eyre::{eyre, Result, WrapErr};
-use lazy_static::lazy_static;
-use ovr_overlay::overlay::{OverlayHandle, OverlayManager};
-use ovr_overlay::Context;
+use ovr_overlay::{
+    overlay::{OverlayHandle, OverlayManager},
+    ColorTint,
+};
 
 pub type Isometry = nalgebra::Isometry3<f32>;
 
@@ -20,7 +19,6 @@ pub struct Bone {
     radius: f32,
     length: f32,
     is_visible: bool,
-    pixel_buf: Box<[u8]>,
 }
 impl Bone {
     pub fn new(
@@ -37,16 +35,15 @@ impl Bone {
             let overlay = mngr
                 .create_overlay(key, key)
                 .wrap_err("Failed to create overlay")?;
-            mngr.set_curvature(overlay, 1.)?;
+            mngr.set_curvature(overlay, 1.)
+                .wrap_err("Failed to set curvature")?;
+            mngr.set_raw_data(overlay, &[255u8; 4], 1, 1, 4)
+                .wrap_err("Failed to set raw data")?;
 
             Ok(overlay)
         };
 
         let overlays = (init_overlay(&keys.0)?, init_overlay(&keys.1)?);
-
-        let pixel_buf = vec![color.red, color.green, color.blue, color.alpha]
-            .repeat(WIDTH_PIXELS * MAX_HEIGHT_PIXELS)
-            .into_boxed_slice();
 
         Ok(Self {
             overlays,
@@ -56,65 +53,64 @@ impl Bone {
             length,
             color,
             is_visible: false,
-            pixel_buf,
         })
     }
 
     pub fn update_render(&self, mngr: &mut OverlayManager<'_>) -> Result<()> {
-        mngr.set_width(self.overlays.0, self.circumference())
-            .and_then(|_| mngr.set_width(self.overlays.1, self.circumference()))
-            .wrap_err("Failed to set radius")?;
+        // Set Color
+        {
+            fn f(color: u8) -> f32 {
+                color as f32 / 255.
+            }
+            let tint = ColorTint {
+                r: f(self.color.r),
+                g: f(self.color.g),
+                b: f(self.color.b),
+                a: f(self.color.a),
+            };
+            mngr.set_tint(self.overlays.0, tint)
+                .and_then(|_| mngr.set_tint(self.overlays.1, tint))
+                .wrap_err("Failed to set color")?;
+        }
+
+        // Set width and height
+        {
+            let mut f = |overlay| -> Result<()> {
+                mngr.set_width(overlay, self.circumference())
+                    .wrap_err("Failed to set radius")?;
+                let aspect = self.circumference() / self.length;
+                mngr.set_texel_aspect(overlay, aspect)
+                    .wrap_err("Failed to set texture aspect ratio")?;
+
+                Ok(())
+            };
+
+            f(self.overlays.0)?;
+            f(self.overlays.1)?;
+        }
 
         mngr.set_visibility(self.overlays.0, self.is_visible)
             .and_then(|_| mngr.set_visibility(self.overlays.1, self.is_visible))
             .wrap_err("Failed to show overlay")?;
 
-        // set height
+        // Set transform
         {
-            let pixel_height = self.pixel_height().unwrap();
-            let slice = &self.pixel_buf[..WIDTH_PIXELS * 4 * pixel_height];
-            mngr.set_raw_data(self.overlays.0, slice, WIDTH_PIXELS, pixel_height, 4)
-                .and_then(|_| {
-                    mngr.set_raw_data(self.overlays.1, slice, WIDTH_PIXELS, pixel_height, 4)
-                })
-                .wrap_err("Failed to set raw texture data")?;
-        }
+            let homo = self.iso.to_homogeneous();
 
-        // TODO: Set position
-        // TODO: Set rotation
-        // TODO: Set length
+            // TODO: Set position
+            // TODO: Set rotation
+        }
 
         Ok(())
-    }
-
-    /// Returns the height in pixels, or `None` if it exceeds the max height
-    fn pixel_height(&self) -> Option<usize> {
-        let pixel_height = self.length * WIDTH_PIXELS as f32 / self.circumference();
-        let pixel_height = f32::round(pixel_height) as usize;
-
-        if pixel_height > MAX_HEIGHT_PIXELS {
-            None
-        } else {
-            Some(pixel_height)
-        }
     }
 
     pub fn set_isometry(&mut self, isometry: Isometry) {
         self.iso = isometry;
     }
 
-    pub fn set_length(&mut self, length: f32) -> Result<()> {
+    pub fn set_length(&mut self, length: f32) {
         assert!(length >= 0., "Length must be positive");
-        let old_len = self.length;
         self.length = length;
-        if self.pixel_height().is_none() {
-            self.length = old_len;
-            return Err(eyre!(
-                "length too long! Maximum length is {}",
-                MAX_HEIGHT_PIXELS
-            ));
-        }
-        Ok(())
     }
 
     pub fn set_radius(&mut self, radius: f32) {
--- src/lib.rs
+++ src/lib.rs
@@ -24,16 +24,16 @@ pub fn main() -> Result<()> {
     let context = ovr::Context::init().wrap_err("Failed to initialize OpenVR")?;
     let mngr = &mut context.overlay_mngr();
 
-    let mut bone_length = 2. * RADIUS;
+    let mut bone_length = RADIUS * 2.;
 
     // Set up overlay
     let mut bone = Bone::new(
         mngr,
         RGBA {
-            red: 255,
-            green: 255,
-            blue: 0,
-            alpha: 20,
+            r: 255,
+            g: 255,
+            b: 0,
+            a: 20,
         },
         Isometry::default(),
         String::from("Bone"),
@@ -46,8 +46,8 @@ pub fn main() -> Result<()> {
 
     log::info!("Main Loop");
     while !stop_signal.load(Ordering::Relaxed) {
-        bone_length *= 2.;
-        bone.set_length(bone_length)?;
+        bone_length *= 1.5;
+        bone.set_length(bone_length);
         bone.update_render(mngr)
             .wrap_err("Could not update render")?;
         std::thread::sleep(Duration::from_millis(1000));
@@ -60,10 +60,10 @@ pub fn main() -> Result<()> {
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct RGBA {
-    red: u8,
-    green: u8,
-    blue: u8,
-    alpha: u8,
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
 }
 
 #[cfg(test)]
