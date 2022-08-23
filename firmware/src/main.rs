bb2c62625c360a4fa0e926cca765fdf5a3cbf6f6
Ryan Butler <thebutlah@gmail.com>
2022-08-23 04:01:01 -0400

    Added embassy/async, defmt, cargo-embed, improved README

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+// Needed for embassy macros
+#![feature(type_alias_impl_trait)]
 
 extern crate alloc;
 
@@ -10,44 +12,87 @@ static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 // Set up backtraces
 use esp_backtrace as _;
 
-use core::fmt::Write;
-use core::mem::MaybeUninit;
+// Set up global defmt logger
+use defmt_rtt as _;
+
+use embassy_executor::{task, Executor};
+use embassy_futures::yield_now;
 use esp32c3_hal::{
     clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Rtc,
 };
 use riscv_rt::entry;
+use static_cell::StaticCell;
+
+// use rtt_target::{rtt_init_print, rprintln};
+use defmt::error;
 
 #[entry]
 fn main() -> ! {
     // Initialize the global allocator BEFORE you use it
     {
-        const HEAP_SIZE: usize = 1024;
+        const HEAP_SIZE: usize = 10240;
         static mut HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
         unsafe { ALLOCATOR.init(HEAP.as_mut_ptr(), HEAP_SIZE) }
     }
 
+    // defmt_serial::defmt_serial(crate::logging::BufferedSerial::take().unwrap());
+    // rtt_init_print!();
+
     let peripherals = Peripherals::take().unwrap();
-    let system = peripherals.SYSTEM.split();
+
+    static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+    EXECUTOR.init(Executor::new()).run(move |spawner| {
+        spawner.spawn(async_main(peripherals)).unwrap();
+        spawner.spawn(sensor_data()).unwrap();
+    });
+}
+
+#[task]
+async fn async_main(p: Peripherals) {
+    // defmt_serial::defmt_serial(crate::logging::BufferedSerial::take().unwrap());
+
+    let system = p.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
     // Disable the RTC and TIMG watchdog timers
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
-    let mut wdt1 = timer_group1.wdt;
+    {
+        let mut rtc = Rtc::new(p.RTC_CNTL);
+        let timer_group0 = TimerGroup::new(p.TIMG0, &clocks);
+        let mut wdt0 = timer_group0.wdt;
+        let timer_group1 = TimerGroup::new(p.TIMG1, &clocks);
+        let mut wdt1 = timer_group1.wdt;
 
-    rtc.swd.disable();
-    rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
+        rtc.rwdt.disable();
+        rtc.swd.disable();
+        // rtc.set_super_wdt_enable(false);
+        // rtc.set_wdt_enable(false);
+        wdt0.disable();
+        wdt1.disable();
+    }
 
-    let mut usb = esp32c3_hal::UsbSerialJtag;
+    // let mut usb = esp32c3_hal::UsbSerialJtag;
     let mut i = 0;
-    let arc = alloc::sync::Arc::new(10u8);
-    // let arc = 1;
     loop {
-        writeln!(&mut usb, "ayyyyy {i} {arc:?}");
+        error!("In main(), i was {}", i);
+        // rprintln!("In main(), i was {}", i);
+        // writeln!(&mut usb, "in main(), i is {i}").unwrap();
         i += 1;
+        yield_now().await
+    }
+}
+
+#[task]
+async fn sensor_data() {
+    let mut i = 0;
+    // let mut delay = embassy_time::Delay;
+    // let mut usb = esp32c3_hal::UsbSerialJtag;
+    loop {
+        error!("In data(), i was {}", i);
+        // rprintln!("In data(), i was {}", i);
+        // writeln!(&mut usb, "in data(), i is {i}").unwrap();
+
+        // DelayUs::delay_ms(&mut delay, 1000).await.unwrap();
+        i += 1;
+        yield_now().await
     }
 }
