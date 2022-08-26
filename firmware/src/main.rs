62e7a733ca621e74d4c1d14a11bd93e7d0881a66
Ryan Butler <thebutlah@gmail.com>
2022-08-25 17:00:09 -0700

    Stopped using async timers, now trying to use defmt

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![no_main]
-// Needed for embassy
+// Needed for embassy macros
 #![feature(type_alias_impl_trait)]
 
 extern crate alloc;
@@ -12,11 +12,14 @@ static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 // Set up backtraces
 use esp_backtrace as _;
 
+// Set up global defmt logger
+use defmt_rtt as _;
+
 use core::fmt::Write;
+use defmt::{debug, error, info};
 use embassy_executor::{task, Executor};
-use embedded_hal_async::delay::DelayUs;
 use esp32c3_hal::{
-    clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, RtcCntl,
+    clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Rtc,
     UsbSerialJtag,
 };
 use riscv_rt::entry;
@@ -32,29 +35,34 @@ fn main() -> ! {
     }
 
     let peripherals = Peripherals::take().unwrap();
-    let system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-
-    // Disable the RTC and TIMG watchdog timers
-    let mut rtc = RtcCntl::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
-    let mut wdt1 = timer_group1.wdt;
-
-    rtc.set_super_wdt_enable(false);
-    rtc.set_wdt_enable(false);
-    wdt0.disable();
-    wdt1.disable();
 
     static EXECUTOR: StaticCell<Executor> = StaticCell::new();
     EXECUTOR
         .init(Executor::new())
-        .run(move |spawner| spawner.spawn(async_main()).unwrap());
+        .run(move |spawner| spawner.spawn(async_main(peripherals)).unwrap());
 }
 
 #[task]
-async fn async_main() {
+async fn async_main(p: Peripherals) {
+    let system = p.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    // Disable the RTC and TIMG watchdog timers
+    {
+        let mut rtc = Rtc::new(p.RTC_CNTL);
+        let timer_group0 = TimerGroup::new(p.TIMG0, &clocks);
+        let mut wdt0 = timer_group0.wdt;
+        let timer_group1 = TimerGroup::new(p.TIMG1, &clocks);
+        let mut wdt1 = timer_group1.wdt;
+
+        rtc.rwdt.disable();
+        rtc.swd.disable();
+        // rtc.set_super_wdt_enable(false);
+        // rtc.set_wdt_enable(false);
+        wdt0.disable();
+        wdt1.disable();
+    }
+
     let mut usb = esp32c3_hal::UsbSerialJtag;
     let mut i = 0;
     loop {
@@ -64,12 +72,13 @@ async fn async_main() {
 }
 
 #[task]
-async fn counter() {
+async fn sensor_data() {
     let mut i = 0;
-    let mut delay = embassy_time::Delay;
+    // let mut delay = embassy_time::Delay;
     let mut usb = UsbSerialJtag;
     loop {
-        DelayUs::delay_ms(&mut delay, 1000).await.unwrap();
+        // DelayUs::delay_ms(&mut delay, 1000).await.unwrap();
+        error!("hello");
         writeln!(&mut usb, "hello world, i is {i}").unwrap();
         i += 1;
     }
