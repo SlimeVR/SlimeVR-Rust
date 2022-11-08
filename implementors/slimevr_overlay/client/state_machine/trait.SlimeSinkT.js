899536741fb73760fc179eebea3f9678f34f0040
TheButlah <thebutlah@gmail.com>
2022-11-08 17:08:18 +0000

    Deploying to docs from @ SlimeVR/SlimeVR-Rust@a6d17a33b91610b85b840b8665d4e0a9f24215ff 🚀

--- implementors/slimevr_overlay/client/state_machine/trait.SlimeSinkT.js
+++ implementors/slimevr_overlay/client/state_machine/trait.SlimeSinkT.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["slimevr_overlay"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"slimevr_overlay":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
