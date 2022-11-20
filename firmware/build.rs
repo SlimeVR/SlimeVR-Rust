e85d6f5495b56ca60238798b9d5722f1032d6c43
ImUrX <urielfontan2002@gmail.com>
2022-11-20 00:46:03 -0300

    more stuff

--- firmware/build.rs
+++ firmware/build.rs
@@ -1,6 +1,6 @@
 use feature_utils::mandatory_and_unique;
 
-mandatory_and_unique!("mcu-esp32c3");
+mandatory_and_unique!("esp32c3");
 mandatory_and_unique!("imu-mpu6050");
 mandatory_and_unique!("log-rtt", "log-usb-serial");
 
