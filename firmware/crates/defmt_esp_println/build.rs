38d79f9cb11a8a5c97855c93ae9e57b2c9f95617
Ryan Butler <thebutlah@gmail.com>
2022-11-22 01:54:08 -0500

    firmware: use `feature_utils` from crates.io (#64)
    Use `feature_utils` from crates.io

--- firmware/crates/defmt_esp_println/build.rs
+++ firmware/crates/defmt_esp_println/build.rs
@@ -1,4 +1,4 @@
-use feature_macros::mandatory_and_unique;
+use feature_utils::mandatory_and_unique;
 
 mandatory_and_unique!("uart", "jtag_serial");
 mandatory_and_unique!("esp32", "esp32c2", "esp32c3", "esp32s2", "esp32s3", "esp8266");
