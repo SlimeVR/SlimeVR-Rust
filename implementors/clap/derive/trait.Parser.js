94b1c7c73b21c44fb11aa573b38dab0e3de0173b
TheButlah <thebutlah@gmail.com>
2022-11-28 05:17:10 +0000

    Deploying to docs from @ SlimeVR/SlimeVR-Rust@982541de6f1d32a93b7d85b6d8fa3fa07b6bc706 🚀

--- implementors/clap/derive/trait.Parser.js
+++ implementors/clap/derive/trait.Parser.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
-"autoupdater":[["impl Parser for <a class=\"struct\" href=\"autoupdater/struct.Args.html\" title=\"struct autoupdater::Args\">Args</a>"]]
+"autoupdater":[["impl Parser for <a class=\"struct\" href=\"autoupdater/struct.Args.html\" title=\"struct autoupdater::Args\">Args</a>"]],
+"slimevr_overlay":[["impl Parser for <a class=\"struct\" href=\"slimevr_overlay/struct.Args.html\" title=\"struct slimevr_overlay::Args\">Args</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
