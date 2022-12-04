294e288c29eed70c787595a01f741166120a743f
Ryan Butler <thebutlah@gmail.com>
2022-12-04 03:14:45 -0500

    fixed log-* always bringing in esp-println

--- firmware/crates/defmt_esp_println/build.rs
+++ firmware/crates/defmt_esp_println/build.rs
@@ -1,6 +1,6 @@
-use feature_utils::mandatory_and_unique;
+use feature_utils::{mandatory_and_unique, unique};
 
-mandatory_and_unique!("uart", "jtag_serial");
+unique!("uart", "jtag_serial");
 mandatory_and_unique!("esp32", "esp32c2", "esp32c3", "esp32s2", "esp32s3", "esp8266");
 
 fn main() {}
