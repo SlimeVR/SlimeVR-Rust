a0da61736c208aec3f671beb1d67c6ca9ee662e4
Ryan Butler <thebutlah@gmail.com>
2022-12-03 23:24:53 -0500

    commiting current attempt to bringup nice nano

--- nrf_demo/src/main.rs
+++ nrf_demo/src/main.rs
@@ -4,13 +4,14 @@
 
 use core::fmt::Write;
 
+use cortex_m::peripheral::SCB;
 use nrf52840_hal::pac::UARTE0;
 use nrf52840_hal::{gpio, uarte, Uarte};
 use panic_halt as _;
 
-use cortex_m_rt::entry;
-use embassy_executor::{task, Executor};
-use embassy_futures::yield_now;
+use cortex_m_rt::{entry, pre_init};
+// use embassy_executor::{task, Executor};
+// use embassy_futures::yield_now;
 use embedded_hal::blocking::delay::DelayMs;
 use nrf52840_hal::prelude::OutputPin;
 use nrf52840_hal::{
@@ -19,6 +20,14 @@ use nrf52840_hal::{
 };
 use static_cell::StaticCell;
 
+// according to gabu#6113 on the nice!nano discord, normally the chip expects the
+// interrupt table at 0x0000. But since our code starts at 0x1000, our interrupt table
+// is there. So we must set the value in the register.
+#[pre_init]
+unsafe fn before_main() {
+	(*SCB::PTR).vtor.write(0x1000);
+}
+
 #[entry]
 fn main() -> ! {
 	let p = nrf52840_hal::pac::Peripherals::take().unwrap();
@@ -26,41 +35,60 @@ fn main() -> ! {
 
 	let p0 = nrf52840_hal::gpio::p0::Parts::new(p.P0);
 	// let p1 = nrf52840_hal::gpio::p1::Parts::new(p.P1);
-	let led = p0.p0_15.into_push_pull_output(Level::Low);
-	let delay = Delay::new(cp.SYST);
-	let uarte = Uarte::new(
-		p.UARTE0,
-		uarte::Pins {
-			txd: p0.p0_10.into_push_pull_output(gpio::Level::High).degrade(),
-			rxd: p0.p0_09.into_floating_input().degrade(),
-			cts: None,
-			rts: None,
-		},
-		uarte::Parity::EXCLUDED,
-		uarte::Baudrate::BAUD115200,
-	);
+	let mut led = p0.p0_15.into_push_pull_output(Level::Low).degrade();
+	let mut delay = Delay::new(cp.SYST);
+	// let mut uarte = Uarte::new(
+	// 	p.UARTE0,
+	// 	uarte::Pins {
+	// 		txd: p0.p0_06.into_push_pull_output(gpio::Level::Low).degrade(),
+	// 		rxd: p0.p0_08.into_floating_input().degrade(),
+	// 		cts: None,
+	// 		rts: None,
+	// 	},
+	// 	uarte::Parity::EXCLUDED,
+	// 	uarte::Baudrate::BAUD115200,
+	// );
 
-	static EXECUTOR: StaticCell<Executor> = StaticCell::new();
-	EXECUTOR.init(Executor::new()).run(move |spawner| {
-		spawner.spawn(main_task(led, delay, uarte)).unwrap();
-	});
-}
-
-#[task]
-async fn main_task(
-	mut led: nrf52840_hal::gpio::p0::P0_15<nrf52840_hal::gpio::Output<PushPull>>,
-	mut delay: Delay,
-	mut uarte: Uarte<UARTE0>,
-) {
+	// Message must be in SRAM
+	let mut buf = [0u8; 64];
 	loop {
-		writeln!(uarte, "Turning on").unwrap();
+		buf.copy_from_slice(b"Turning on\r\n");
+		// uarte.write(&buf).unwrap();
 		// Yes, these should be async, but I was lazy
-		delay.delay_ms(1000 as u16);
+		delay.delay_ms(2000 as u16);
 		led.set_high().expect("Failed to set high");
 		delay.delay_ms(1000 as u16);
-		writeln!(uarte, "Turning off").unwrap();
+		buf.copy_from_slice(b"Turning off\r\n");
+		// uarte.write(&buf).unwrap();
 
 		led.set_low().expect("Failed to set low");
-		yield_now().await
 	}
+
+	// static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+	// EXECUTOR.init(Executor::new()).run(move |spawner| {
+	// 	spawner.spawn(main_task(led, delay, uarte)).unwrap();
+	// });
 }
+
+// #[task]
+// async fn main_task(
+// 	mut led: nrf52840_hal::gpio::Pin<nrf52840_hal::gpio::Output<PushPull>>,
+// 	mut delay: Delay,
+// 	mut uarte: Uarte<UARTE0>,
+// ) {
+// 	// Message must be in SRAM
+// 	let mut buf = [0u8; 64];
+// 	loop {
+// 		buf.copy_from_slice(b"Turning on\r\n");
+// 		uarte.write(&buf).unwrap();
+// 		// Yes, these should be async, but I was lazy
+// 		delay.delay_ms(4000 as u16);
+// 		// led.set_high().expect("Failed to set high");
+// 		delay.delay_ms(1000 as u16);
+// 		buf.copy_from_slice(b"Turning off\r\n");
+// 		uarte.write(&buf).unwrap();
+//
+// 		// led.set_low().expect("Failed to set low");
+// 		yield_now().await
+// 	}
+// }
