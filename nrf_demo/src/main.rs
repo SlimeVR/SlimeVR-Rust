8f3456d490b01875335c5482a36222e5c7f42012
Ryan Butler <thebutlah@gmail.com>
2022-12-04 01:48:19 -0500

    firmware: nrf bringup (#91)
    * commiting current attempt to bringup nice nano
    
    * It now blinks
    
    * Finally fully working

--- nrf_demo/src/main.rs
+++ nrf_demo/src/main.rs
@@ -2,17 +2,19 @@
 #![no_std]
 #![feature(type_alias_impl_trait)]
 
-use core::fmt::Write;
-
-use nrf52840_hal::pac::UARTE0;
-use nrf52840_hal::{gpio, uarte, Uarte};
+use defmt_rtt as _;
 use panic_halt as _;
 
-use cortex_m_rt::entry;
 use embassy_executor::{task, Executor};
 use embassy_futures::yield_now;
-use embedded_hal::blocking::delay::DelayMs;
+use nrf52840_hal::pac::UARTE0;
 use nrf52840_hal::prelude::OutputPin;
+use nrf52840_hal::{gpio, uarte, Uarte};
+
+use cortex_m_rt::entry;
+// use embassy_executor::{task, Executor};
+// use embassy_futures::yield_now;
+use embedded_hal::blocking::delay::DelayMs;
 use nrf52840_hal::{
 	gpio::{Level, PushPull},
 	Delay,
@@ -25,14 +27,13 @@ fn main() -> ! {
 	let cp = nrf52840_hal::pac::CorePeripherals::take().unwrap();
 
 	let p0 = nrf52840_hal::gpio::p0::Parts::new(p.P0);
-	// let p1 = nrf52840_hal::gpio::p1::Parts::new(p.P1);
-	let led = p0.p0_15.into_push_pull_output(Level::Low);
+	let led = p0.p0_15.into_push_pull_output(Level::Low).degrade();
 	let delay = Delay::new(cp.SYST);
 	let uarte = Uarte::new(
 		p.UARTE0,
 		uarte::Pins {
-			txd: p0.p0_10.into_push_pull_output(gpio::Level::High).degrade(),
-			rxd: p0.p0_09.into_floating_input().degrade(),
+			txd: p0.p0_06.into_push_pull_output(gpio::Level::Low).degrade(),
+			rxd: p0.p0_08.into_floating_input().degrade(),
 			cts: None,
 			rts: None,
 		},
@@ -43,24 +44,33 @@ fn main() -> ! {
 	static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 	EXECUTOR.init(Executor::new()).run(move |spawner| {
 		spawner.spawn(main_task(led, delay, uarte)).unwrap();
-	});
+	})
 }
 
 #[task]
 async fn main_task(
-	mut led: nrf52840_hal::gpio::p0::P0_15<nrf52840_hal::gpio::Output<PushPull>>,
+	mut led: nrf52840_hal::gpio::Pin<nrf52840_hal::gpio::Output<PushPull>>,
 	mut delay: Delay,
 	mut uarte: Uarte<UARTE0>,
 ) {
+	// Message must be in SRAM
+	const LEN: usize = 13;
+	const ON: &[u8; LEN] = b"Turning on \r\n";
+	const OFF: &[u8; LEN] = b"Turning off\r\n";
+	let mut buf = [0u8; LEN];
+
 	loop {
-		writeln!(uarte, "Turning on").unwrap();
 		// Yes, these should be async, but I was lazy
-		delay.delay_ms(1000 as u16);
+		buf.copy_from_slice(ON);
+		uarte.write(&buf).unwrap();
 		led.set_high().expect("Failed to set high");
 		delay.delay_ms(1000 as u16);
-		writeln!(uarte, "Turning off").unwrap();
 
+		buf.copy_from_slice(OFF);
+		uarte.write(&buf).unwrap();
 		led.set_low().expect("Failed to set low");
+		delay.delay_ms(500 as u16);
+
 		yield_now().await
 	}
 }
