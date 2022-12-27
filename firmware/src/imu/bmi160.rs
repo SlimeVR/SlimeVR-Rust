1f7c556bf20acf90713e62af37792fd01d934090
Ryan Butler <thebutlah@gmail.com>
2022-12-26 19:50:35 -0600

    tmp

---
+++ firmware/src/imu/bmi160.rs
@@ -0,0 +1,82 @@
+use super::{Imu, ImuKind, Quat};
+use crate::aliases::I2c;
+use crate::utils;
+
+use defmt::{debug, trace};
+use embedded_hal::blocking::delay::DelayMs;
+use embedded_hal::blocking::i2c::{Write, WriteRead};
+use firmware_protocol::ImuType;
+
+type BmiDriver<I2c> = ::bmi160::Bmi160<bmi160::interface::I2cInterface<I2c>>;
+// Second generic is `()` because we don't have chip select errors in I2C.
+type BmiError<I: I2c> = ::bmi160::Error<<I as I2c>::Error, ()>;
+
+pub struct InitError<I: I2c> {
+	pub i2c: I,
+	pub error: BmiError<I>,
+}
+impl<I2c, E> core::fmt::Debug for InitError<I2c>
+where
+	I2c: WriteRead<Error = E> + Write<Error = E>,
+	E: core::fmt::Debug,
+{
+	fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+		self.error.fmt(f)
+	}
+}
+
+pub struct Bmi160<I: I2c> {
+	driver: BmiDriver<I>,
+}
+impl<I: I2c> Bmi160<I> {
+	pub fn new(i2c: I, delay: &mut impl DelayMs<u32>) -> Result<Self, InitError<I>> {
+		debug!("Constructing BMI160...");
+		let addr = ::bmi160::SlaveAddr::Default;
+		debug!("I2C address: {:x}", addr.0);
+
+		utils::retry(
+			4,
+			i2c,
+			|mut i2c| {
+				delay.delay_ms(100);
+				trace!("Flushing I2C with bogus data");
+				let _ = i2c.write(addr.0, &[0]);
+				delay.delay_ms(100);
+				trace!("Constructing IMU");
+				let mut driver = BmiDriver::new_with_i2c(i2c, addr);
+				let id = match driver.chip_id() {
+					Ok(id) => id,
+					Err(error) => {
+						return InitError {
+							i2c: driver.destroy(),
+							error,
+						}
+					}
+				};
+				debug!("Constructed BMI with chip id: {}", id);
+				delay.delay_ms(100);
+				Ok(Self { driver })
+			},
+			|i| debug!("Retrying IMU connection (attempts so far: {})", i + 1),
+		)
+		// Map converts from tuple -> struct
+		.map_err(|(i2c, error)| InitError { i2c, error })
+	}
+}
+
+impl<I: I2c> Imu for Bmi160<I> {
+	type Error = BmiError<I>;
+
+	const IMU_TYPE: ImuType = ImuType::Bmi160;
+
+	fn quat(&mut self) -> nb::Result<Quat, Self::Error> {
+		todo!()
+	}
+}
+
+pub fn new_imu(
+	i2c: impl crate::aliases::I2c,
+	delay: &mut impl DelayMs<u32>,
+) -> impl crate::imu::Imu {
+	Bmi160::new(i2c, delay).expect("Failed to initialize BMI160")
+}
--- firmware/src/imu/mod.rs
+++ firmware/src/imu/mod.rs
@@ -9,6 +9,10 @@ mod ඞ {
 #[path = "mpu6050.rs"]
 mod ඞ;
 
+#[cfg(feature = "imu-bmi160")]
+#[path = "bmi160.rs"]
+mod ඞ;
+
 use crate::utils::{nb2a, Unreliable};
 
 use defmt::{debug, info, trace};
@@ -17,6 +21,32 @@ use embedded_hal::blocking::delay::DelayMs;
 use firmware_protocol::ImuType;
 
 pub type Quat = nalgebra::UnitQuaternion<f32>;
+
+pub const IMU_KIND: ImuKind = {
+	#[cfg(any(feature = "imu-mpu6050", feature = "imu-stubbed"))]
+	let v = ImuKind::Mpu6050;
+	#[cfg(feature = "imu-bmi160")]
+	let v = ImuKind::Bmi160;
+	v
+};
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum ImuKind {
+	Mpu6050,
+	Bmi160,
+}
+impl ImuKind {
+	/// The ID this imu type corresponds to in the firmware protocol.
+	///
+	/// Reference:
+	/// https://github.com/SlimeVR/SlimeVR-Tracker-ESP/blob/cb188cfd7a757fa1fda/src/consts.h#L26
+	pub const fn protocol_id(&self) -> u8 {
+		match self {
+			ImuKind::Mpu6050 => 6,
+			ImuKind::Bmi160 => 8,
+		}
+	}
+}
 
 pub trait Imu {
 	type Error: core::fmt::Debug;
