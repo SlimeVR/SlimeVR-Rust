8bad726f5ab2a317de5139d49498862077ea81f5
Ryan Butler <thebutlah@gmail.com>
2023-01-26 04:25:10 -0500

    Refactor to no longer feature gate imu driver modules

--- firmware/src/imu/drivers/mod.rs
+++ firmware/src/imu/drivers/mod.rs
@@ -1,14 +1,3 @@
-mod stubbed;
-
-#[cfg(feature = "imu-stubbed")]
-pub mod ඞ {
-	pub use crate::imu::drivers::stubbed::*;
-}
-
-#[cfg(feature = "imu-mpu6050")]
-#[path = "mpu6050.rs"]
-pub mod ඞ;
-
-#[cfg(feature = "imu-bmi160")]
-#[path = "bmi160/mod.rs"]
-pub mod ඞ;
+pub mod bmi160;
+pub mod mpu6050;
+pub mod stubbed;
