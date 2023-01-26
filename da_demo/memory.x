8b2ef07ab0bef72f576e8ba43cab8dc0e57fc196
Uriel <urielfontan2002@gmail.com>
2023-01-26 07:10:32 -0300

    Start da blinky (#148)
    * start da blinky
    
    * progress
    
    * fix pin
    
    * add embed file
    
    * Update da_demo/src/main.rs
    
    Co-authored-by: Ryan Butler <thebutlah@gmail.com>
    
    * Update da_demo/rust-toolchain.toml
    
    Co-authored-by: Ryan Butler <thebutlah@gmail.com>

---
+++ da_demo/memory.x
@@ -0,0 +1,6 @@
+MEMORY
+{
+  /* NOTE K = KiBi = 1024 bytes */
+  FLASH : ORIGIN = 0x07FC0000, LENGTH = 32K
+  RAM   : ORIGIN = 0x07FC8000, LENGTH = 16K
+}
---
+++ da_demo/src/main.rs
@@ -0,0 +1,34 @@
+#![no_main]
+#![no_std]
+
+// Ensures that critical-section impls are linked against.
+use cortex_m as _;
+use defmt_rtt as _;
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+use cortex_m::delay::Delay;
+use da14531_hal::{gpio::p0::Parts,hal::digital::v2::{OutputPin, PinState}};
+use defmt::error;
+
+#[entry]
+fn main() -> ! {
+    let p = da14531_hal::pac::Peripherals::take().unwrap();
+    let cp = da14531_hal::pac::CorePeripherals::take().unwrap();
+
+    let p0 = Parts::new(p.GPIO);
+    let mut led = p0.p0_09.degrade().into_output(PinState::Low);
+
+    let mut delay = Delay::with_source(cp.SYST, 32000000, cortex_m::peripheral::syst::SystClkSource::Core);
+    let mut i = 0;
+    loop {
+        error!("turning led ON {}", i);
+        led.set_high().expect("failed to set HIGH");
+        delay.delay_ms(1000u32);
+
+        error!("turning led OFF {}", i);
+        led.set_low().expect("failed to set LOW");
+        delay.delay_ms(500u32);
+        i += 1;
+    }
+}
