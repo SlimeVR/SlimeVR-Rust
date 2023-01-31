c2d4a0e22f45aa33726cdda79321eafb0e0adf69
Ryan Butler <thebutlah@gmail.com>
2023-01-31 01:38:16 -0500

    Add DCM fusion (#173)
    A DCM Based Attitude Estimation Algorithm for Low-Cost MEMS IMUs
    Heikki Hyyti & Arto Visala, 2015
    
    Co-authored-by: Aaro Perämaa <aaro.peramaa@gmail.com>

---
+++ firmware/src/imu/fusion/dcm.rs
@@ -0,0 +1,40 @@
+use dcmimu::DCMIMU;
+use embassy_time::Instant;
+use firmware_protocol::ImuType;
+
+use crate::imu::{FusedData, Fuser, Imu, Quat, UnfusedData};
+
+/// Extended Kalman filtering in direction cosine matrix formation
+pub struct Dcm {
+	dcm: DCMIMU,
+	last: Instant,
+}
+
+impl Dcm {
+	pub fn new() -> Self {
+		Self {
+			dcm: DCMIMU::new(),
+			last: Instant::now(),
+		}
+	}
+}
+
+impl Fuser for Dcm {
+	fn process(&mut self, unfused: &UnfusedData) -> FusedData {
+		let last = self.last;
+		self.last = Instant::now();
+		let elapsed = self.last - last;
+
+		let UnfusedData { accel, gyro } = unfused;
+
+		// TODO: Check that these euler angle convention matches
+		let (euler, _) = self.dcm.update(
+			(gyro.x, gyro.y, gyro.z),
+			(accel.x, accel.y, accel.z),
+			elapsed.as_micros() as f32 / 1_000_000.0,
+		);
+
+		let q = Quat::from_euler_angles(euler.roll, euler.pitch, euler.roll);
+		FusedData { q }
+	}
+}
