2fe670da14b40438e2203d674bec99b6d283dfde
Ryan Butler <thebutlah@gmail.com>
2023-02-06 03:28:25 -0500

    GATT demo

--- firmware/src/networking/ble/esp.rs
+++ firmware/src/networking/ble/esp.rs
@@ -4,13 +4,14 @@ use bleps::{
 		create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED,
 		LE_GENERAL_DISCOVERABLE,
 	},
-	Ble, Data, HciConnector,
+	attribute_server::AttributeServer,
+	Ble, HciConnector,
 };
-use defmt::trace;
+use defmt::{debug, error, trace, warn};
 use embassy_futures::yield_now;
 use esp_wifi::{self, ble::controller::BleConnector, current_millis};
 
-pub async fn network_task(packets: &Packets) -> ! {
+pub async fn network_task(_packets: &Packets) -> ! {
 	// HCI is the host-controller interface, which lets the MCU communicate to the BLE hardware through a standard
 	// command interface
 	let connector = BleConnector {};
@@ -18,32 +19,72 @@ pub async fn network_task(packets: &Packets) -> ! {
 	let mut ble = Ble::new(&hci);
 
 	ble.init().expect("Failed to initialize BLE hardware");
+	debug!("Initialized BLE");
 
 	// This will configure settings for the advertising server, like interval rate
 	ble.cmd_set_le_advertising_parameters()
 		.expect("Failed to configure advertising");
 
 	ble.cmd_set_le_advertising_data(create_advertising_data(&[
-		// Public advertising and and we do not support full Bluetooth connection (basic rate/extended data rate)
+		// Public advertising and and we do not support Bluetooth classic
 		AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+		// If we specify the service uuids, it will show up in LightBlue as "1 service"
+		// AdStructure::ServiceUuids16(&[Uuid::Uuid16(0x1809)]),
 		AdStructure::CompleteLocalName("SlimeVR-Rust"),
-		// Placeholder for SlimeVR advertised quaternion data. BLE advertising data size is tight, we should consider a
-		// quantized quaternion format. 64 or even 32 bits should be enough
-		// TODO: Unimplemented
-		// AdStructure::ManufacturerSpecificData {
-		// 	company_identifier: 0,
-		// 	payload: &[0; 16],
-		// },
 	]))
 	.expect("Failed to set advertising data");
 
 	ble.cmd_set_le_advertise_enable(true)
 		.expect("Failed to start advertising");
 
+	let mut rf = || &b"Hello Bare-Metal BLE"[..];
+	let mut wf = |offset: u16, data: &[u8]| {
+		trace!("RECEIVED: {} {=[u8]:a}", offset, data);
+	};
+
+	// let mut wf2 = |offset: u16, data: &[u8]| {
+	// 	trace!("RECEIVED: {} {=[u8]:a}", offset, data);
+	// };
+	//
+	// let mut rf3 = || &b"Hola!"[..];
+	// let mut wf3 = |offset: u16, data: &[u8]| {
+	// 	trace!("RECEIVED: Offset {}, data {=[u8]:a}", offset, data);
+	// };
+
+	use bleps_macros::gatt;
+
+	gatt!([service {
+		uuid: "133712e0-2354-11eb-9f10-fbc30a62cf38",
+		characteristics: [
+			characteristic {
+				uuid: "13370000-2354-11eb-9f10-fbc30a62cf38",
+				read: rf,
+				write: wf,
+			},
+			// characteristic {
+			// 	uuid: "13371111-2354-11eb-9f10-fbc30a62cf38",
+			// 	write: wf2,
+			// },
+			// characteristic {
+			// 	name: "my_characteristic",
+			// 	uuid: "13372222-2354-11eb-9f10-fbc30a62cf38",
+			// 	notify: true,
+			// 	read: rf3,
+			// 	write: wf3,
+			// },
+		],
+	},]);
+
+	let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes);
+
+	debug!("Starting BLE loop");
 	loop {
 		yield_now().await;
-		let Some(event) = ble.poll() else { continue };
-
-		trace!("BLE: {}", event);
+		use bleps::attribute_server::WorkResult;
+		match srv.do_work() {
+			Ok(WorkResult::DidWork) => {}
+			Ok(WorkResult::GotDisconnected) => warn!("BLE Disconnected!"),
+			Err(err) => error!("Err: {}", defmt::Debug2Format(&err)),
+		};
 	}
 }
